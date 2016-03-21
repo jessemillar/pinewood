17e14a56e6175eb771e8c49e1ea9087f2adb79e5
Jesse Millar <hellojessemillar@gmail.com>
2016-03-21 11:58:04 -0600

    Working template

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,4 @@
-(function ($) {
-
-    'use strict';
-
-    // Toggle classes in body for syncing sliding animation with other elements
-    $('#bs-example-navbar-collapse-2')
-        .on('show.bs.collapse', function (e) {
-            $('body').addClass('menu-slider');
-        })
-        .on('shown.bs.collapse', function (e) {
-            $('body').addClass('in');
-        })
-        .on('hide.bs.collapse', function (e) {
-            $('body').removeClass('menu-slider');
-        })
-        .on('hidden.bs.collapse', function (e) {
-            $('body').removeClass('in');
-        });
-
-})(jQuery);
+$("#menu-toggle").click(function(e) {
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
+});
