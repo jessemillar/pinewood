d862b476131e0201983321ed6f72cd63c231c0ee
Jesse Millar <hellojessemillar@gmail.com>
2016-03-23 14:34:59 -0600

    Tweaking chat and adding fun-stats

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,7 @@ $("#menu-toggle").click(function(e) {
   e.preventDefault();
   $("#wrapper").toggleClass("toggled");
 });
+
+var switchPage = function(page) { // For eventually turning the project into a one-page app
+  console.log(page);
+};
