0343f707b0ec479782e123705fd28b26e0c08536
Jesse Millar <hellojessemillar@gmail.com>
2016-03-23 13:51:12 -0600

    The chat page mostly works and chat messages are simulated

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,50 @@
+var users = ["janice", "jesse", "mark", "jeremy", "matthew", "joe", "mike", "dan"]; // A list of fake users for testing and simulation
+var emoji = ["😬", "👍", "😃", "😄", "😅", "🙂", "🙃", "😛", "😎"]; // The allowed emoji for chatting
+
 $("#menu-toggle").click(function(e) {
   e.preventDefault();
   $("#wrapper").toggleClass("toggled");
 });
+
+// Simulate chatting users
+var messageChance = 0.25;
+
+setInterval(function() {
+  if (Math.random() <= messageChance) {
+    postMessage(emoji[Math.round(Math.random() * (emoji.length - 1))]);
+  }
+}, 1000);
+
+var chatModal = function() {
+  var modalContent = "<table class='emoji-table'>";
+
+  for (var i = 0; i < 3; i++) {
+    modalContent += "<tr>";
+    for (var j = 0; j < 3; j++) {
+      var emojiID = i * 3 + j;
+      modalContent += "<td class='emoji' onclick='selectEmoji(" + emojiID + ")'>" + emoji[emojiID] + "</td>";
+    }
+  }
+
+  modalContent += "</table>";
+
+  swal({
+    title: "",
+    text: modalContent,
+    html: true,
+    showConfirmButton: false
+  });
+};
+
+var selectEmoji = function(emojiID) {
+  swal.close();
+  postMessage(emoji[emojiID]);
+};
+
+var postMessage = function(message) {
+  var ul = document.getElementById("chat-bubbles");
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(message));
+  li.setAttribute("class", "list-group-item chat-bubble");
+  ul.appendChild(li);
+};
