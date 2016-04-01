eb055faa8ea4d4774ab3a173b57c88afb5722afb
Jesse Millar <hellojessemillar@gmail.com>
2016-03-31 18:29:56 -0600

    Adding IDs to login inputs

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,3 @@ $("#hamburger").click(function(e) {
   e.preventDefault();
   $("#wrapper").toggleClass("toggled");
 });
-
-var switchPage = function(page) { // For eventually turning the project into a one-page app
-  console.log(page);
-};
