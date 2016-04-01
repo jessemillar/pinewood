8e29cce86522b7761e91a9df674ae06257b20c78
Jesse Millar <hellojessemillar@gmail.com>
2016-03-31 19:02:04 -0600

    Fixing bugs

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,6 @@
 var checkLogin = function() {
   if (document.getElementById("username").value.length > 0 && document.getElementById("password").value.length > 0) {
-    window.location.href = "/pinewood/pages/register";
+    window.location.href = "/pinewood/pages/standings";
   } else {
     sweetAlert("Oops...", "Please enter all information!", "error");
   }
