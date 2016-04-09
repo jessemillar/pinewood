b628ca460b591fb6a128cc65021e91cb78d181cf
Jesse Millar <hellojessemillar@gmail.com>
2016-04-08 20:37:30 -0600

    Working on profile page and social button

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,31 @@ $("#hamburger").click(function(e) {
     $("#wrapper").toggleClass("toggled");
 });
 
-var shareSocial = function() {
-    swal({
-        title: "Share on social media?",
-        text: "<img class='social-icon' onclick=shareOn('Facebook') src='/pinewood/images/facebook.svg'><img class='social-icon' onclick=shareOn('Twitter') src='/pinewood/images/twitter.svg'><img class='social-icon' onclick=shareOn('Instagram') src='/pinewood/images/instagram.svg'>",
-        confirmButtonText: "Cancel",
-        confirmButtonColor: "#c0c0c0",
-        html: true
-    });
+var shareSocial = function(message) {
+    if (!message) {
+        swal({
+            title: "Share on social media?",
+            text: "<img class='social-icon' onclick=shareOn('Facebook') src='/pinewood/images/facebook.svg'><img class='social-icon' onclick=shareOn('Twitter') src='/pinewood/images/twitter.svg'><img class='social-icon' onclick=shareOn('Instagram') src='/pinewood/images/instagram.svg'>",
+            confirmButtonText: "Cancel",
+            confirmButtonColor: "#c0c0c0",
+            html: true
+        });
+    } else {
+        swal({
+            title: "Share " + message + " on social media?",
+            text: "<img class='social-icon' onclick=shareOn('Facebook') src='/pinewood/images/facebook.svg'><img class='social-icon' onclick=shareOn('Twitter') src='/pinewood/images/twitter.svg'><img class='social-icon' onclick=shareOn('Instagram') src='/pinewood/images/instagram.svg'>",
+            confirmButtonText: "Cancel",
+            confirmButtonColor: "#c0c0c0",
+            html: true
+        });
+    }
 };
 
 var shareOn = function(network) {
     swal("Success!", "Shared on " + network + "!", "success");
+};
+
+var profile = function(cub) {
+    console.log("Opening profile for " + cub)
+    window.open("/pinewood/pages/profiles/" + cub, "_self");
 };
